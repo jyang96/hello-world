decbf2e41495b5b6f78dd025b47ba93d2631ad5d
jyang96 <55048601+jyang96@users.noreply.github.com>
2019-09-11 18:28:31 +0800

    Add files via upload

--- EpipolarGeometry.py
+++ EpipolarGeometry.py
@@ -1,19 +1,42 @@
+#%%
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img1 = cv2.imread('C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64\Calib Images\Test.bmp',0)  #queryimage # left image
-img2 = cv2.imread('C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64\Calib Images\TestR.bmp',0) #trainimage # right image
+input = "C:\\Users\jxyan\hello-world\in\\"
+output = "C:\\Users\jxyan\hello-world\out\\"
 
-sift = cv2.SIFT()
+img1 = cv2.imread(input + "Test.bmp",0)  #queryimage # left image
+img2 = cv2.imread(input + "TestR.bmp",0) #trainimage # right image
+cv2.imshow("Right",img2)
+cv2.waitKey(2000)
+cv2.destroyAllWindows()
 
+#%%
+orb = cv2.ORB_create(nfeatures=1500)
+
+kp1, des1 = orb.detectAndCompute(img1, None)
+kp2, des2 = orb.detectAndCompute(img2, None)
+
+img1 = cv2.drawKeypoints(img1, kp1, None)
+cv2.imshow("Image 1 Features", img1)
+cv2.waitKey(2000)
+cv2.destroyAllWindows()
+
+#%%
+
+#sift = cv.SIFT()
 # find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1,None)
-kp2, des2 = sift.detectAndCompute(img2,None)
+#kp1, des1 = sift.detectAndCompute(img1,None)
+#kp2, des2 = sift.detectAndCompute(img2,None)
 
 # FLANN parameters
 FLANN_INDEX_KDTREE = 0
-index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+FLANN_INDEX_LSH = 6
+index_params = index_params= dict(algorithm = FLANN_INDEX_LSH,
+                   table_number = 6, # 12
+                   key_size = 12,     # 20
+                   multi_probe_level = 1) #2
 search_params = dict(checks=50)
 
 flann = cv2.FlannBasedMatcher(index_params,search_params)
@@ -25,27 +48,28 @@ pts2 = []
 
 # ratio test as per Lowe's paper
 for i,(m,n) in enumerate(matches):
-    if m.distance < 0.8*n.distance:
+    if m.distance < 0.7*n.distance:
         good.append(m)
         pts2.append(kp2[m.trainIdx].pt)
         pts1.append(kp1[m.queryIdx].pt)
 
+#%%
 # Finding fundamental matrix
 pts1 = np.int32(pts1)
 pts2 = np.int32(pts2)
-F, mask = cv2.findFundamentalMat(pts1,pts2,cv2.FM_LMEDS)
+F, mask = cv2.findFundamentalMat(pts1.astype(float),pts2.astype(float),cv2.FM_LMEDS)
 
 # We select only inlier points
 pts1 = pts1[mask.ravel()==1]
 pts2 = pts2[mask.ravel()==1]
 
+#%%
 # Find epilines
 def drawlines(img1,img2,lines,pts1,pts2):
     ''' img1 - image on which we draw the epilines for the points in img2
         lines - corresponding epilines '''
-    r,c = img1.shape
-    img1 = cv2.cvtColor(img1,cv2.COLOR_GRAY2BGR)
-    img2 = cv2.cvtColor(img2,cv2.COLOR_GRAY2BGR)
+    r = img1.shape[0]
+    c = img1.shape[1]
     for r,pt1,pt2 in zip(lines,pts1,pts2):
         color = tuple(np.random.randint(0,255,3).tolist())
         x0,y0 = map(int, [0, -r[2]/r[1] ])
@@ -56,12 +80,12 @@ def drawlines(img1,img2,lines,pts1,pts2):
     return img1,img2
 
 # Find epilines corresponding to points in right image (second image) and drawing its lines on left image
-lines1 = cv2.computeCorrespondEpilines(pts2.reshape(-1,1,2), 2,F)
+lines1 = cv2.computeCorrespondEpilines(pts2.reshape(-1,1,2).astype('float32'), 2,F)
 lines1 = lines1.reshape(-1,3)
 img5,img6 = drawlines(img1,img2,lines1,pts1,pts2)
 
 # Find epilines corresponding to points in left image (first image) and drawing its lines on right image
-lines2 = cv2.computeCorrespondEpilines(pts1.reshape(-1,1,2), 1,F)
+lines2 = cv2.computeCorrespondEpilines(pts1.reshape(-1,1,2).astype('float32'), 1,F)
 lines2 = lines2.reshape(-1,3)
 img3,img4 = drawlines(img2,img1,lines2,pts2,pts1)
 
